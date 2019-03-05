09fd4100eb160b4d0a8fbc194305e63cf54b9a9d
daivik <daivik@ad3.ucdavis.edu@pc5.cs.ucdavis.edu>
2019-03-04 21:44:46 -0800

    Makefile complete, Phase 1 done but unmount bug exists

--- libfs/fs.c
+++ libfs/fs.c
@@ -33,73 +33,185 @@ struct Root{
 
 	char filename[16]; //16 byte file name string
 	uint32_t fileSize; //4 bytes
-	uint16_t dataIndex; //2 bytes for index of first data block
+	uint16_t dataIndex; //2 bytes for ind ex of first data block
 	char rootPadding[10]; //10 unused bytes of padding
 
 }__attribute__((packed)); //packed attributes to pack memory blocks into one continuous chunk
 
 
-
+struct Superblock super;// superblock
+struct Fatblock* Fatarray; //FAT array to store all FAT blocks
+struct Root* root; //root array to hold file entries
 
 
 
 int fs_mount(const char *diskname)
 {
 	/* TODO: Phase 1 */
+	char sigCheck[8] = "ECS150FS"; //Array signature check
+	
+	if(block_disk_open(diskname) == -1) //If opening virtual disk block fails
+		return -1; //Failure in mounting FS
+
+	else
+	{
+		if(block_read(0, &super) == -1) //Reading content of 1st virtual block into superblock
+			return -1;//failure
+
+		for(int i=0; i<8; i++) //traversal to compare signature of superblock with required ECS150FS file system signature
+			if(!(super.signature[i] == sigCheck[i]))
+				return -1; //Failure in finding right file system
+		
+		if(!(block_disk_count() == super.blockTotal)) //Total amount of virtual blocks check
+			return -1; //Failed check
+		
+		Fatarray = (struct Fatblock*) malloc((super.fatBlock)*BLOCK_SIZE); //Allocating memory for the Fat array to store an array of structs of BLOCK SIZE with number of fatblocks supplied by the superblock 	
+		
+		root =  (struct Root*) malloc(sizeof(struct Root)*FS_FILE_MAX_COUNT); //Allocating memory for roo dir
+
+		for(int index=0; index<super.fatBlock; index++)
+			if((block_read((index+1), (&Fatarray[(BLOCK_SIZE/2)*index]))) == -1) //Reading content of virtual block into fatblock array
+				return -1;//failure
+
+		if((block_read((super.rootIndex), root)) == -1) //Reading content of virtual block into root
+			return -1;//failure
+	}
+
+	return 0;//Success
 }
+
 
 int fs_umount(void)
 {
 	/* TODO: Phase 1 */
+
+	if(block_write(0, &super) == -1) //Writing out superblock to virtual disk
+		return -1;
+
+	for(int index=0; index<super.fatBlock; index++)
+		if((block_write((index+1), (&Fatarray[(BLOCK_SIZE/2)*index]))) == -1) //Writing content into virtual block from fatblock array
+			return -1;//failure
+
+	if(block_write((super.rootIndex), root) == -1) //Writing out rootblock to virtual disk
+		return -1; //failure
+
+	free(Fatarray); //Deallocating memory set for Fat array
+	free(root); //Deallocating memory set for root dir array
+
+	if(block_disk_close() == -1) //If virtual disk was not opened 
+		return -1; //Failure	
+
+	return 0; //Success
 }
+
 
 int fs_info(void)
 {
 	/* TODO: Phase 1 */
+
+	int zeroCount = 0; //Count for Fat block zeros
+	int nullCount = 0; //Count for Root directory null filename entries
+
+	if(block_disk_close() == -1) //If virtual disk wasn't opened
+		return -1;
+	else
+	{
+		printf("FS Info:\n");
+		printf("total_blk_count=%d\n", super.blockTotal);
+		printf("fat_blk_count=%d\n", super.fatBlock);
+		printf("rdir_blk=%d\n", super.rootIndex);
+		printf("data_blk=%d\n", super.dataIndex);
+		printf("data_blk_count=%d\n", super.dataTotal);
+
+		for(int i=0; i<super.dataTotal; i++)
+		{
+			if(Fatarray[i].wordFat == 0)
+				zeroCount++;	
+
+		}
+		
+		for(int j=0; j<FS_FILE_MAX_COUNT; j++)
+		{
+			if(root[j].filename[0] == '\0')
+				nullCount++;
+		}
+
+		printf("fat_free_ratio=%d/%d\n", zeroCount, super.dataTotal);
+		printf("rdir_free_ratio=%d/%d\n", nullCount, FS_FILE_MAX_COUNT);
+	}
+
+	return 0;//success
+
 }
 
 int fs_create(const char *filename)
 {
 	/* TODO: Phase 2 */
+
+
+
+
+	return 0;
 }
 
 int fs_delete(const char *filename)
 {
 	/* TODO: Phase 2 */
+
+
+
+	return 0;
 }
 
 int fs_ls(void)
 {
 	/* TODO: Phase 2 */
+
+
+	return 0;
+
 }
 
 int fs_open(const char *filename)
 {
 	/* TODO: Phase 3 */
+
+
+	return 0;
 }
 
 int fs_close(int fd)
 {
 	/* TODO: Phase 3 */
+
+	return 0;
 }
 
 int fs_stat(int fd)
 {
 	/* TODO: Phase 3 */
+
+	return 0;
 }
 
 int fs_lseek(int fd, size_t offset)
 {
 	/* TODO: Phase 3 */
+
+	return 0;
 }
 
 int fs_write(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
+
+	return 0;
 }
 
 int fs_read(int fd, void *buf, size_t count)
 {
 	/* TODO: Phase 4 */
+
+	return 0;
 }
 
